d5ad96510587bd54cc7fe94361de176852eb0e40
Rahul Aggarwal <rahulaggarwal13500@gmail.com>
2022-03-16 23:17:58 +0530

    organizer page implemented

--- frontend/src/screens/pre_election/PreElectionScreen.js
+++ frontend/src/screens/pre_election/PreElectionScreen.js
@@ -1,39 +1,39 @@
 import React from "react";
 import { Routes, Route, Outlet } from "react-router-dom";
 import AboutScreen from "./AboutScreen";
-import Footer from "../../components/Footer/Footer";
 import NominationScreen from "./NominationScreen";
 import TopNav from "../../components/Home/TopNav/TopNav";
-import BlockchainMessage from "../../components/Home/BlockchainMessage/BlockchainMessage"
-import HomeRouteNavBar from "../../components/Home/HomeRouteNavbar/HomeRouteNavBar"
+import BlockchainMessage from "../../components/Home/BlockchainMessage/BlockchainMessage";
+import HomeRouteNavBar from "../../components/Home/HomeRouteNavbar/HomeRouteNavBar";
 import StatsScreen from "./StatsScreen";
 import RulesScreen from "./RulesScreen";
 import OrganisersScreen from "./OrganisersScreen";
+import Footer from "../../components/Footer/Footer";
 const Layout = () => {
   return (
     <div className="md:ml-20 mt-12 mr-4 ml-4">
-      <Outlet/>
+      <Outlet />
     </div>
   );
-}
+};
 const PreElectionScreen = () => {
   return (
     <>
       <div>
         {/* Pre-election navbar */}
-        <TopNav/>
-        <BlockchainMessage/>
+        <TopNav />
+        <BlockchainMessage />
         {/* Routes Navbar */}
         <div className="mt-5 ml-1 mr-1 md:ml-16 md:mr-10">
-          <HomeRouteNavBar/>
+          <HomeRouteNavBar />
         </div>
         <Routes>
-          <Route path="/" element={<Layout/>}>
-          <Route path="/" exact element={<AboutScreen />} />
+          <Route path="/" element={<Layout />}>
+            <Route path="/" exact element={<AboutScreen />} />
             <Route path="nominations/*" exact element={<NominationScreen />} />
-            <Route path="stats" exact element={<StatsScreen/>} />
-            <Route path="rules" exact element={<RulesScreen/>} />
-            <Route path="organisers" exact element={<OrganisersScreen/>} />
+            <Route path="stats" exact element={<StatsScreen />} />
+            <Route path="rules" exact element={<RulesScreen />} />
+            <Route path="organisers" exact element={<OrganisersScreen />} />
           </Route>
         </Routes>
       </div>
