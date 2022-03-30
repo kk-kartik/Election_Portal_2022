89a472baccdc40bc68f491dae55d8d5e0a96db2b
Yashi Natu <yashiii@pop-os.localdomain>
2022-03-30 13:14:37 +0530

    fetching text editor content as html

--- frontend/src/components/Nominate/AgendaList/AgendaList.js
+++ frontend/src/components/Nominate/AgendaList/AgendaList.js
@@ -3,8 +3,21 @@ import styles from "./AgendaList.module.css";
 import dots from "./three-dots.svg";
 import TextEditor from "../../TextEditor/TextEditor"
 import Agenda from "./Agenda";
+import draftToHtml from 'draftjs-to-html';
+import convertToRaw from 'draftjs-to-html';
 const AgendaList = () => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const [title, setTitle] = useState ('')
+  const [htmlContent, setHtmlContent] = useState('')
+
+    const getContent = (htmlContentProp) => {
+        setHtmlContent(htmlContentProp);
+        console.log(htmlContentProp);
+    }
+
+
+
   return (
     <div className="">
      
@@ -19,6 +32,7 @@ const AgendaList = () => {
               counter these unfortunate incidents, I propose, to install CCTV
               camera in major places of campus, skvbsdkvb skdjvbdaouhv skdj skdjksjbdvadv askja asjaskj aksjak cdhd " long={true} />
       {isOpen && <TextEditor setIsOpen={setIsOpen} />}
+      {/* <textarea value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}></textarea> */}
       <div className="pt-8">
         <button className={`text-white ${styles.button} px-5`} onClick={() => setIsOpen(true)}>
           {" "}
--- frontend/src/components/TextEditor/TextEditor.js
+++ frontend/src/components/TextEditor/TextEditor.js
@@ -1,15 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { Editor } from "react-draft-wysiwyg";
 import { EditorState } from "draft-js";
+
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import styles from "./TextEditor.module.css"
+import { convertToHTML } from 'draft-convert';
+
 const TextEditor = (props) => {
   const [editorState, setEditorState] = useState(() =>
-    EditorState.createEmpty()
-  );
-  useEffect(() => {
-    console.log(editorState);
-  }, [editorState]);
+  EditorState.createEmpty()
+);
+
+const [convertedContent, setConvertedContent] = useState('');
+  const handleEditorChange = (state) => {
+    setEditorState(state);
+    convertContentToHTML();
+    // console.log((editorState.getCurrentContent().getBlocksAsArray));
+    // sendContent();
+};
+const convertContentToHTML = () => {
+  let currentContentAsHTML = convertToHTML(editorState.getCurrentContent());
+  setConvertedContent(currentContentAsHTML);
+  console.log(currentContentAsHTML);
+}
+
+const [title, setTitle] = useState ('')
+
+
+const onSubmit = (e) => {
+  e.preventDefault();
+
+  if (!title) {
+      alert('Please add a title')
+      return;
+  }
+}
+
   return (
       <div className={styles.darkBG}>
      <div className={styles.centered}> 
@@ -22,17 +48,20 @@ const TextEditor = (props) => {
     </div>
     <div className={` ${styles.container} p-2`}>
     <div className="pb-2">
-        <input type="text" placeholder="Title" className={` ${styles.input} p-2 w-full`}/>
+        <input type="text" placeholder="Title" className={` ${styles.input} p-2 w-full`} value={title} onChange={(e) => setTitle(e.target.value)} />
     </div>
       <div className={` ${styles.editor} p-2`}>
         <Editor
           editorState={editorState}
-          onEditorStateChange={setEditorState}
+          onEditorStateChange={handleEditorChange}
           placeholder="Write about your agenda here..."
+          
         />
+        {/* <textarea value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}></textarea> */}
+
       </div>
       <div className="flex flex-row-reverse p-2">
-         <button className={` ${styles.button} px-11 py-2.5 font-semibold disabled disabled:bg-violet-100`}> Save </button>
+         <button className={` ${styles.button} px-11 py-2.5 font-semibold disabled disabled:bg-violet-100`}  onClick={ onSubmit}> Save </button>
       </div>
     </div>
     </div>
