f2ca8fcc3b9ec4f7c21167d5d756e0dadcaaf385
VolDemorT-acer <kunalsolanke1000@gmail.com>
2022-04-01 12:18:00 +0530

    Added outlook login button and refresh api call

--- frontend/src/screens/election/Vote.js
+++ frontend/src/screens/election/Vote.js
@@ -3,6 +3,9 @@ import { blindVote, encryptVote, verifySign } from "../../lib/encrypt";
 import { getNEpair } from "../../lib/fetch";
 import { signVote } from "../../lib/post";
 
+import MicrosoftLogin from "react-microsoft-login";
+import axios from "axios";
+
 function Vote() {
   const [vote, setVote] = useState(null);
   const [blindData, setblindData] = useState({});
@@ -11,6 +14,31 @@ function Vote() {
   const [NEpair, setNEpair] = useState(null);
   const [error, setError] = useState(null);
   const [isVerified, setVerified] = useState(false);
+
+  const authHandler = async (err, data) => {
+    console.log(data, err);
+    const accessToken = data["accessToken"];
+    const res = await axios.get(
+      "http://localhost:8000/elections_api/auth/outlook/signin/",
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        withCredentials: true,
+      }
+    );
+
+    const res1 = await axios.get(
+      "http://localhost:8000/elections_api/auth/token/refresh/",
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        withCredentials: true,
+      }
+    );
+    console.log(res1);
+  };
 
   const handleChange = (e) => {
     setVote(e.target.value);
@@ -31,7 +59,7 @@ function Vote() {
     console.log("Encrypted data", encryptedData);
     const blindedData = blindVote(encryptedData, keys_res.data);
     console.log("Blinded data", blindedData.blindedVote);
-    console.log(blindedData.r)
+    console.log(blindedData.r);
     const sign_res = await signVote(blindedData.blindedVote);
     console.log("Sign api response", sign_res);
     if (sign_res.isError) {
@@ -45,7 +73,7 @@ function Vote() {
       sign_res.data.signed,
       keys_res.data
     );
-    console.log(isVerified)
+    console.log(isVerified);
     if (isVerified) {
       setVerified(true);
     }
@@ -56,6 +84,16 @@ function Vote() {
   };
   return (
     <div className="mt-20 mx-20">
+      <MicrosoftLogin
+        clientId={"f2dc9516-b846-4460-9c99-c46cf28b869b"}
+        graphScopes={["6974450a-7569-4f53-ab04-47aa8b84687f/read"]}
+        redirectUri={"http://localhost:3000/"}
+        authCallback={authHandler}
+        tenantUrl={
+          "https://login.microsoftonline.com/850aa78d-94e1-4bc6-9cf3-8c11b530701c"
+        }
+        authCallback={authHandler}
+      />
       <form onSubmit={handleSubmit}>
         <label>Enter your vote</label>
         <input
