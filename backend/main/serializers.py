848fcb13d7d8aeac30927528d49347d5ebaccc44
Saket Kumar <saketkumar1202@gmail.com>
2022-04-03 17:57:38 +0530

    Finished backend except auth and user and minor fixes in swagger needed.

--- backend/main/serializers.py
+++ backend/main/serializers.py
@@ -10,3 +10,25 @@ class PositionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Position
         fields ='__all__'
+
+class EuserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EUser
+        fields =['id','name','roll_number','degree','hostel','branch','email']
+
+class CandidateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Candidate
+        exclude = ['election']
+        depth = 1
+
+class CandidatePostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Candidate
+        fields ='__all__'
+        # exclude = ['image','agenda_pdf']
+
+class FaqSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Faq
+        fields ='__all__'
