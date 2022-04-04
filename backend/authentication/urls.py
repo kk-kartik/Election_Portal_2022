cdfb37dcfd7b21aca2bca0487bb1b6512154945f
VolDemorT-acer <kunalsolanke1000@gmail.com>
2022-04-04 10:36:41 +0530

    add verify route

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
+   path("token/verify/",views.TokenVerifyView.as_view(),name="token_verify"),
    path('', include('dj_rest_auth.urls')),
-   path('registration/', include('dj_rest_auth.registration.urls')),
    path("social/google/",view=views.GoogleLogin.as_view(),name="google_login"),
    path("social/outlook/",view=views.MicrosoftLogin.as_view(),name="outlook_login"),
    path('accounts/', include('allauth.urls'), name='socialaccount_signup'),
--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,7 +1,18 @@
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.microsoft.views import MicrosoftGraphOAuth2Adapter
 from dj_rest_auth.registration.views import SocialLoginView
+from dj_rest_auth.jwt_auth import JWTCookieAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework  import status
+from rest_framework.response import Response
+from rest_framework.generics import GenericAPIView
 
+class TokenVerifyView(GenericAPIView):
+    authentication_classes = [JWTCookieAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request):
+        return Response({"ok":True,"detail":"User is logged in!"},status=status.HTTP_200_OK)
 
 class GoogleLogin(SocialLoginView): # if you want to use Implicit Grant, use this
     adapter_class = GoogleOAuth2Adapter
