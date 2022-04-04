648de7f90cbbe064ade78724c89a4adfb14e8871
VolDemorT-acer <kunalsolanke1000@gmail.com>
2022-04-04 11:48:24 +0530

    Add roll number as email

--- backend/main/models.py
+++ backend/main/models.py
@@ -130,4 +130,4 @@ class Statistic(models.Model):
 @receiver(post_save,sender=User)
 def create_euser(sender,instance,created,*args,**kwargs):
     if created:
-        euser = EUser.objects.create(user=instance,email=instance.email,name=instance.first_name,roll_number=instance.last_name)
+        euser = EUser.objects.create(user=instance,email=instance.email,name=instance.first_name,roll_number=instance.email)
