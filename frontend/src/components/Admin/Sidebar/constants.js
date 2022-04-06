a495d03c36711137f764ad15404a313e7d9b3e9c
Sarthak Diwan <s.diwan@iitg.ac.in>
2022-04-06 20:48:51 +0530

    Admin candidates

--- frontend/src/components/Admin/Sidebar/constants.js
+++ frontend/src/components/Admin/Sidebar/constants.js
@@ -1,5 +1,6 @@
 export const SideNavList = [
   { name: "Important Dates", url: "importantdates" },
   { name: "Debates", url: "debates" },
+  { name: "Candidates", url: "candidates" },
   { name: "FAQ", url: "faq" },
 ];
