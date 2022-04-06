98bb90a5eeb1e6abac95ec544a6a7314eca38678
Yashi Natu <yashiii@pop-os.localdomain>
2022-04-06 19:30:29 +0530

    candidate nomination screen

--- frontend/src/components/Home/Nomination/AgendaList/AgendaList.js
+++ frontend/src/components/Home/Nomination/AgendaList/AgendaList.js
@@ -3,19 +3,27 @@ import React from "react";
 const AgendaList = () => {
   return (
     <>
-      <div className="font-semibold text-lg">My Top Three Agendas</div>
-      <div className="text-gray-600 text-lg">
+      <div className="font-semibold text-xl pb-4">My Top Three Agendas</div>
+      <div className="p-2 leading-8">
+      <div className="text-gray-600 text-xl ">
         1. Setting up Career Counselling Cell (opprotunitites in non-tech, UPSC,
         GATE, GRE and post doctoral in foreign universities.
       </div>
-      <div className="text-gray-600 text-lg">
-        1. Setting up Career Counselling Cell (opprotunitites in non-tech, UPSC,
+      <div className="text-gray-600 text-xl">
+        2. Setting up Career Counselling Cell (opprotunitites in non-tech, UPSC,
         GATE, GRE and post doctoral in foreign universities.
       </div>
-      <div className="text-gray-600 text-lg">
-        1. Setting up Career Counselling Cell (opprotunitites in non-tech, UPSC,
+      <div className="text-gray-600 text-xl">
+        3. Setting up Career Counselling Cell (opprotunitites in non-tech, UPSC,
         GATE, GRE and post doctoral in foreign universities.
       </div>
+      </div>
+      <div className="font-semibold text-xl py-4">About me</div>
+      <div className="text-gray-600 text-xl p-2 leading-8">
+      Currently serving as Superintendent of Police (Communication), Assam. ​An Engineering graduate in the field Electronics and Communication). ​​Was one of the SIT members to investigate the infamous 30th October, 2008 serial blasts took place in Guwahati and various districts of lower Assam. ​DGP’s commendation Medal.
+​CM’s Police Medal for outstanding service. ​Internal Security Medal from MHA, Govt. of India.
+      </div> 
+
     </>
   );
 };
--- frontend/src/screens/pre_election/CandidateNominationScreen.js
+++ frontend/src/screens/pre_election/CandidateNominationScreen.js
@@ -2,13 +2,26 @@ import React from "react";
 import BreadCrumbs from "../../components/Home/Nomination/BreadCrumbs/BreadCrumbs";
 import NameTag from "../../components/Home/Nomination/NameTag/NameTag";
 import AgendaList from "../../components/Home/Nomination/AgendaList/AgendaList";
-
+import YoutubeEmbed from "../../components/Home/Nomination/Video/YoutubeEmbed";
+import ProfileCard from "../../components/Home/Nomination/ProfileCard/profileCard";
 const CandidateNominationScreen = () => {
   return (
     <>
+    <br />
+    <div className="pl-16">
       <BreadCrumbs />
+    </div>
+    <div className="ml-auto mr-auto w-full md:w-1/2 ">
+      <div className="p-4 pt-16"> 
+   <YoutubeEmbed embedId="rokGy0huYEA" />
+      </div>
+      <div className="py-8">
+
       <NameTag />
+      </div>
       <AgendaList />
+      <ProfileCard />
+    </div>
     </>
   );
 };
