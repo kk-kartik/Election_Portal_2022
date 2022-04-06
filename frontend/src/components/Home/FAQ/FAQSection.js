f677d72a348bdaee79d5216f86fb05a1ca2a22c2
Yashi Natu <yashiii@pop-os.localdomain>
2022-04-06 23:00:54 +0530

    bug fix

--- frontend/src/components/Home/FAQ/FAQSection.js
+++ frontend/src/components/Home/FAQ/FAQSection.js
@@ -1,5 +1,4 @@
 import React, {useEffect} from "react";
-import { FAQData } from "./FAQData";
 import FAQCard from "./FAQCard";
 import { useDispatch, useSelector } from "react-redux";
 import { getFaq, deleteFAQ } from "../../../actions/faq";
