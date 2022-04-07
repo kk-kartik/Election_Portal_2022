f159e0b961d0907e2f2b221486f12a725861465d
VolDemorT-acer <kunalsolanke1000@gmail.com>
2022-04-08 00:57:47 +0530

    update access token lifetime

--- backend/election_portal22/settings.py
+++ backend/election_portal22/settings.py
@@ -191,7 +191,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
