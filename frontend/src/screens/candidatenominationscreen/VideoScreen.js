d9983e4239a67c57d5892cac47fc49ee3a1cbf15
Yashi Natu <yashiii@pop-os.localdomain>
2022-04-11 13:56:16 +0530

    candidate profile

--- frontend/src/screens/candidatenominationscreen/VideoScreen.js
+++ frontend/src/screens/candidatenominationscreen/VideoScreen.js
@@ -16,16 +16,25 @@ const VideoScreen = () => {
     candidate,
     setError,
     updateNomination,
+    isNominationComplete,
+    isDeadlineOver,
+    isFormClosed,
   } = useNominate();
 
   const videoRef = useRef(null);
-  let embedId = '';
-  const CheckURL=(url) => {
-    var regExp = /^https?\:\/\/(?:www\.youtube(?:\-nocookie)?\.com\/|m\.youtube\.com\/|youtube\.com\/)?(?:ytscreeningroom\?vi?=|youtu\.be\/|vi?\/|user\/.+\/u\/\w{1,2}\/|embed\/|watch\?(?:.*\&)?vi?=|\&vi?=|\?(?:.*\&)?vi?=)([^#\&\?\n\/<>"']*)/i;
-      var match = url.match(regExp);
-      return (match && match[1].length==11)? match[1] : false;
-  }
+  let embedId = "";
+  const CheckURL = (value) => {
+    var regEx =
+      "^(?:https?:)?//[^/]*(?:youtube(?:-nocookie)?.com|youtu.be).*[=/]([-\\w]{11})(?:\\?|=|&|$)";
+    var matches = value.match(regEx);
+    if (matches) {
+      return matches[1];
+    }
+    return false;
+  };
   const submitData = async () => {
+    console.log(videoRef.current.value);
+    console.log(CheckURL(videoRef.current.value));
     if (!videoRef.current.value || !CheckURL(videoRef.current.value)) {
       setError("Please add valid url");
       return;
@@ -33,14 +42,14 @@ const VideoScreen = () => {
     embedId = CheckURL(videoRef.current.value);
     console.log(embedId);
     const data = { video: videoRef.current.value };
-
     updateNomination(data, "/nominate/witnesses");
+    setError(null);
   };
   return (
     <>
-      <p>Enter Youtube Video url.</p>
-      <label for="video" className="font-normal mb-4 text-sm text-gray-800">
-        Video Url
+      {/* <p>Enter Youtube Video url.</p> */}
+      <label for="video" className="font-medium mb-4 text-base text-gray-800">
+        Enter Youtube Video Url :
       </label>
       <br />
       <input
@@ -51,17 +60,31 @@ const VideoScreen = () => {
         className={`${styles.input} md:w-1/2 lg:w-2/5 w-full`}
         defaultValue={candidate?.video}
         ref={videoRef}
+        disabled={isFormClosed}
       />
-      <div className="w-full md:w-1/2">
-       <YoutubeEmbed embedId={embedId}/>
-      </div>
-      <SaveAndNext
-        error={error}
-        message={message}
-        loading={loading}
-        submit={submitData}
-      />
-      
+      {candidate.video && (
+        <div className="w-full md:w-1/2 my-4">
+          <YoutubeEmbed embedId={CheckURL(candidate.video)} />
+        </div>
+      )}
+
+      {!isFormClosed && (
+        <SaveAndNext
+          error={error}
+          message={message}
+          loading={loading}
+          submit={submitData}
+        />
+      )}
+      {isNominationComplete ? (
+        <p className="text-blue-500">Your Nomination is complete</p>
+      ) : (
+        <>
+          {isDeadlineOver && (
+            <p className="text-blue-500">Nomination Deadline is over.</p>
+          )}
+        </>
+      )}
     </>
   );
 };
