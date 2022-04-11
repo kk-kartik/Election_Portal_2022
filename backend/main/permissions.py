8eb3606fa5355392554c4d5d6e6349457ed7b793
VolDemorT-acer <kunalsolanke1000@gmail.com>
2022-04-11 15:16:44 +0530

    close form

--- backend/main/permissions.py
+++ backend/main/permissions.py
@@ -90,7 +90,7 @@ class OnlyOrganizerOrCandidate(permissions.BasePermission):
         
         return is_organizer or is_candidate
 
-basic_info_deadline = 1649662251052+2.5*60*60*1000
+basic_info_deadline = 1649662251052+2*60*60*1000
 nomination_deadline = 1649662251052+6*60*60*1000
 
 class CandidateDeadlinePermissions(permissions.BasePermission):
