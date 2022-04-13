31d3a2ccd64750934cce161a31179ba4ace318c4
VolDemorT-acer <kunalsolanke1000@gmail.com>
2022-04-13 20:30:04 +0530

    open candiate nomination form

--- frontend/src/screens/candidatenominationscreen/AboutScreen.js
+++ frontend/src/screens/candidatenominationscreen/AboutScreen.js
@@ -23,7 +23,7 @@ import { set } from "react-hook-form";
 const deadline = 1649734200000;
 // const deadline = 1649549824000;
 const checkDeadline = () => {
-  return new Date(Date.now()).getTime() >= deadline;
+  return false;
 };
 
 export const candidateSchema = yup.object().shape({
