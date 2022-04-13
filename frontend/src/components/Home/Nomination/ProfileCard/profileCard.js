30b9e32e12c1bea7920961e90ea198673d54cbe8
Sarthak Bhagwat <sarthak_bhagwat@pop-os.localdomain>
2022-04-13 20:03:56 +0530

    fix:youtube embed aspect ratio

--- frontend/src/components/Home/Nomination/ProfileCard/profileCard.js
+++ frontend/src/components/Home/Nomination/ProfileCard/profileCard.js
@@ -1,42 +1,43 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { AgendaSVG } from "../NameTag/AgendaSVG";
-import styles from "./ProfileCard.module.css"
+import styles from "./ProfileCard.module.css";
 import pic from "./profilepic.svg";
-import styles2 from "../../../../screens/CandidatePositionForm/CandidatePositionForm.module.css"
+import styles2 from "../../../../screens/CandidatePositionForm/CandidatePositionForm.module.css";
 import ShareModal from "../ShareModal/ShareModal";
 const ProfileCard = (props) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <div className={`mr-10 mt-6 ${styles.container} p-4`}>
-          <div className="flex pb-2">
-              <div className="pr-4">
-                <img src={props.image} className="w-20 h-20"/>
-              </div>
-              <div>
-                <div className="pb-1 text-xl"> {props.name}</div>
-                <div className="text-lg">{props.degree}, {props.branch}</div>
-                <div className="text-lg"> IIT Guwahati </div>
-              </div>
+        <div className="flex pb-2">
+          <div className="pr-4">
+            <img src={props.image} className="w-auto h-20" alt="profile" />
           </div>
-          <div className="bg-gray-100 p-4">
-            <p className="italic text-xl text-gray-600">“{props.tagline}”</p>
+          <div>
+            <div className="pb-1 text-xl"> {props.name}</div>
+            <div className="text-lg">
+              {props.degree}, {props.branch}
+            </div>
+            <div className="text-lg"> IIT Guwahati </div>
           </div>
-        <div className="flex space-x-2  hidden sm:block py-6">
-        {isOpen && (
-          <ShareModal setIsOpen={setIsOpen} url={window.location.href} />
-        )}
-            <button className={styles2.button} onClick={() => setIsOpen(true)}>
-              Share this profile
-            </button>
-            <button className={styles2.button3}>
-              <div className="flex items-center">
-                Agenda
-                <AgendaSVG />
-              </div>
-            </button>
         </div>
-       
+        <div className="bg-gray-100 p-4">
+          <p className="italic text-xl text-gray-600">“{props.tagline}”</p>
+        </div>
+        <div className="flex space-x-2  hidden sm:block py-6">
+          {isOpen && (
+            <ShareModal setIsOpen={setIsOpen} url={window.location.href} />
+          )}
+          <button className={styles2.button} onClick={() => setIsOpen(true)}>
+            Share this profile
+          </button>
+          <button className={styles2.button3}>
+            <div className="flex items-center">
+              Agenda
+              <AgendaSVG />
+            </div>
+          </button>
+        </div>
       </div>
     </>
   );
--- frontend/src/screens/pre_election/CandidateNominationScreen.js
+++ frontend/src/screens/pre_election/CandidateNominationScreen.js
@@ -1,55 +1,69 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import BreadCrumbs from "../../components/Home/Nomination/BreadCrumbs/BreadCrumbs";
 import NameTag from "../../components/Home/Nomination/NameTag/NameTag";
 import AgendaList from "../../components/Home/Nomination/AgendaList/AgendaList";
 import YoutubeEmbed from "../../components/Home/Nomination/Video/YoutubeEmbed";
 import ProfileCard from "../../components/Home/Nomination/ProfileCard/profileCard";
-import svg from "../../components/Nominate/AgendaList/style.svg"
-import svg2 from "../../components/Nominate/AgendaList/style2.svg"
-import styles from "../Register/RegisterScreen.module.css"
-import {getCandidateByID} from "../../api/index";
+import svg from "../../components/Nominate/AgendaList/style.svg";
+import svg2 from "../../components/Nominate/AgendaList/style2.svg";
+import styles from "../Register/RegisterScreen.module.css";
+import { getCandidateByID } from "../../api/index";
 
 const CandidateNominationScreen = () => {
-
   const [loaded, setLoaded] = useState(null);
   // const getData = async (id) => {
   //    data = await getCandidateByID(id);
   //    return data
   // }
-  let id = window.location.href.match('candidate\/([0-9]+)')[1];
-  let tr = <div>
-    Loading</div>;
-  if(loaded == null){
-    getCandidateByID(id).then((data)=>{console.log("---data---"); setLoaded(data.data); console.log(data.data)});
+  let id = window.location.href.match("candidate/([0-9]+)")[1];
+  let tr = <div>Loading</div>;
+  if (loaded == null) {
+    getCandidateByID(id).then((data) => {
+      console.log("---data---");
+      setLoaded(data.data);
+      console.log(data.data);
+    });
     return tr;
   }
   const getEmbedID = (value) => {
     var regEx =
       "^(?:https?:)?//[^/]*(?:youtube(?:-nocookie)?.com|youtu.be).*[=/]([-\\w]{11})(?:\\?|=|&|$)";
     var matches = value.match(regEx);
-    return matches[1]; 
+    return matches[1];
   };
   return (
     <div className="m-2">
       <br />
       <div className="md:pl-16 pl-4">
-        <BreadCrumbs name={loaded.name} position={loaded.position}/>
+        <BreadCrumbs name={loaded.name} position={loaded.position} />
       </div>
       <div className="ml-auto mr-auto w-full md:w-1/2 ">
-        <div className="p-4 pt-16 flex items-end">
+        <div className="pt-16 pb-10 relative items-end">
           <YoutubeEmbed embedId={getEmbedID(loaded.video)} />
+
           <div className={`absolute z-40  ${styles.svgs}`}>
-            <img src={svg} className="mr-4" />
-            <img src={svg2} className="ml-6" />
+            <img src={svg} className="mr-4" alt="." />
+            <img src={svg2} className="ml-6" alt="." />
           </div>
         </div>
         <div className="py-8">
-          <NameTag name = {loaded.name} branch={loaded.branch} position={loaded.position} degree={loaded.degree}/>
+          <NameTag
+            name={loaded.name}
+            branch={loaded.branch}
+            position={loaded.position}
+            degree={loaded.degree}
+          />
         </div>
-        <AgendaList agenda={loaded.agenda_text} about={loaded.about}/>
+        <AgendaList agenda={loaded.agenda_text} about={loaded.about} />
         <br />
         <br />
-        <ProfileCard name = {loaded.name} branch={loaded.branch} tagline={loaded.tagline} image={loaded.image} degree={loaded.degree}/>
+        <ProfileCard
+          name={loaded.name}
+          branch={loaded.branch}
+          tagline={loaded.tagline}
+          image={loaded.image}
+          degree={loaded.degree}
+        />
       </div>
     </div>
   );
