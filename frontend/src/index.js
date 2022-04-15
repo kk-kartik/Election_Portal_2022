d6a76cf143b1dbd00be6dd393580b3cca022131d
Bhargav-saikia1999 <bhargavsaikia06@gmail.com>
2022-04-15 11:19:46 +0530

    minor change

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,34 +1,34 @@
 import React from "react";
-import {render,hydrate} from "react-dom";
+import ReactDOM, { render, hydrate } from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 import store from "./store";
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    rootElement
-  );
-} else {
-  render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    rootElement
-  );
-}
+// const rootElement = document.getElementById("root");
+// if (rootElement.hasChildNodes()) {
+//   hydrate(
+//     <Provider store={store}>
+//       <App />
+//     </Provider>,
+//     rootElement
+//   );
+// } else {
+//   render(
+//     <Provider store={store}>
+//       <App />
+//     </Provider>,
+//     rootElement
+//   );
+// }
 
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById("root")
-// );
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
