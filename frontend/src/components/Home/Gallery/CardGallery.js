ecd5302ad107f22723226607649889e935b9d660
Bhargav-saikia1999 <bhargavsaikia06@gmail.com>
2022-04-17 16:25:55 +0530

    shadow -> scale

--- frontend/src/components/Home/Gallery/CardGallery.js
+++ frontend/src/components/Home/Gallery/CardGallery.js
@@ -9,7 +9,7 @@ const CardGallery = ({persons}) => {
 
   return (
     <div className="ml-4 flex items-center justify-center md:m-3">
-      <div className="grid grid-flow-col md:grid-flow-row-dense	auto-cols-max overflow-auto md:grid-cols-2 lg:grid-cols-3 gap-3">
+      <div className="grid grid-flow-col md:grid-flow-row-dense	auto-cols-max overflow-auto sm:overflow-visible md:grid-cols-2 lg:grid-cols-3 gap-3">
         {persons && persons.map((person,i)=>{
           return <Card key={i} id={i} person={person} className={styles.card}/>;
         })}
--- frontend/src/screens/pre_election/AboutScreen.js
+++ frontend/src/screens/pre_election/AboutScreen.js
@@ -12,7 +12,7 @@ const AboutScreen = () => {
       </Helmet>
       <Register />
       <Dates />
-      {/* <Debates /> */}
+      <Debates />
     </>
   );
 };
