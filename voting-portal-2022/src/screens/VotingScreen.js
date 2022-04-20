ac58f4d8b4a0989499310426ef232dca85a2b041
Pawaneet56 <pawaneetsingh56@gmail.com>
2022-04-20 21:24:20 +0530

    web3 code

--- voting-portal-2022/src/screens/VotingScreen.js
+++ voting-portal-2022/src/screens/VotingScreen.js
@@ -4,6 +4,14 @@ import { useParams } from "react-router-dom";
 import { pos2idMap } from "../constants";
 import { getCandidateByPos } from "../redux/actions/candidates";
 import { Link } from "react-router-dom";
+import web3 from "../ethereum/webThree";
+import election from "../ethereum/election";
+const Tx = require("ethereumjs-tx").Transaction;
+
+const publicKey = "0xD0e203A04Eb4024Fbd90768b46E37aC67F1Cd707";
+const privateKey =
+  "9f94794beb1b094dfa4dd85f1190703500e5179fe4b53767dcfc785eaa4620b0";
+const contractAddress = "0x4e6a5bfb44c6d243a44ed5f6704be50c38ac289f";
 
 const VotingScreen = () => {
   const dispatch = useDispatch();
@@ -12,18 +20,61 @@ const VotingScreen = () => {
 
   useEffect(() => {
     dispatch(getCandidateByPos(pos2idMap[position], position));
+
+    // Web3 transaction start
+    const countVotes = async () => {
+      const counts = await election.methods.voterCount().call();
+      console.log(counts);
+    };
+    //counting total vote casted
+    countVotes();
+
+    //voting start. Votes contain encrypted vote and voterid
+    const methodFunction = async () => {
+      const votes = "123";
+      const voterId = "12";
+
+      const functionAbi = election.methods.store(voterId, votes).encodeABI();
+
+      web3.eth.getTransactionCount(publicKey, function (err, nonce) {
+        var details = {
+          from: web3.utils.toChecksumAddress(publicKey),
+          nonce: web3.utils.toHex(nonce),
+          gasPrice: web3.utils.toHex(web3.utils.toWei("100", "gwei")),
+          gasLimit: 500000,
+          to: contractAddress,
+          value: 0,
+          data: functionAbi,
+        };
+
+        var tx = new Tx(details, { chain: "rinkeby" });
+        tx.sign(Buffer.from(privateKey, "hex"));
+        var serializedTx = tx.serialize();
+
+        web3.eth
+          .sendSignedTransaction("0x" + serializedTx.toString("hex"))
+          .on("receipt", console.log)
+          .on("error", console.log);
+      });
+    };
+
+    methodFunction();
+    //Web3 part over
   }, [position]);
 
-  console.log("votingScreen candidates: ",candidates);
+  console.log("votingScreen candidates: ", candidates);
 
   return (
     <div className="p-1">
       <p>VotingScreen for {position}</p>
       <p className="mb-4">This is the main screen for voting</p>
-      <Link to="/swc" className="p-2 mt-3 bg-blue-700 text-white">Link to vote for SWC (just for test)</Link>
+      <Link to="/swc" className="p-2 mt-3 bg-blue-700 text-white">
+        Link to vote for SWC (just for test)
+      </Link>
       <div className="p-6">
         <h2 className="text-lg underline">
-          Candidate list for <span className="font-bold">{position}</span> position:
+          Candidate list for <span className="font-bold">{position}</span>{" "}
+          position:
         </h2>
         <ul>
           {candidates &&
