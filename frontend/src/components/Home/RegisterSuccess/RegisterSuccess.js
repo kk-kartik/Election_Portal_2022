a8fbbeb17effeebc1fd231703b9ac7e4857d0c20
VolDemorT-acer <kunalsolanke1000@gmail.com>
2022-04-23 04:33:21 +0530

    Fix voter card place

--- frontend/src/components/Home/RegisterSuccess/RegisterSuccess.js
+++ frontend/src/components/Home/RegisterSuccess/RegisterSuccess.js
@@ -1,85 +1,74 @@
-import styles from "./RegisterSuccess.module.css"
-import Icon from "./icon.svg"
+import styles from "./RegisterSuccess.module.css";
+import Icon from "./icon.svg";
 import { useSelector, useDispatch } from "react-redux";
-import DEPT from "../../../constants/depts"
+import DEPT from "../../../constants/depts";
 import getUserImg from "../../../actions/getUserImg";
-import {useEffect, useState} from "react";
-import DefaultIMG from "./default.svg"
+import { useEffect, useState } from "react";
+import DefaultIMG from "./default.svg";
 
 const RegisterSuccess = (props) => {
-    const userData = useSelector((store) => store.auth);
-    const userImg = useSelector((store) => store.getUserImg);
-    const dispatch = useDispatch();
-    useEffect(() => {
-        let data = {"email":userData?.euser?.email}
-        dispatch(getUserImg(data));
-      }, [dispatch, userData]);
-    // console.log("userData", userData);
-    console.log("userImg", userImg);
-    let hid = ""
-    if(userData == null || userData?.euser?.registration_complete == false){
-        hid = "hidden";
+  const userData = useSelector((store) => store.auth);
+  const userImg = useSelector((store) => store.getUserImg);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    let data = { email: userData?.euser?.email };
+    dispatch(getUserImg(data));
+  }, [dispatch, userData]);
+  // console.log("userData", userData);
+  console.log("userImg", userImg);
+  let hid = "";
+  if (userData == null || userData?.euser?.registration_complete == false) {
+    hid = "hidden";
+  }
+  let roll_no = "";
+  for (let i = 0; i < userData?.euser?.roll_number.length; i++) {
+    if (i % 3 == 0) {
+      roll_no += " ";
     }
-    let roll_no = "";
-    for(let i=0; i<userData?.euser?.roll_number.length; i++){
-        if(i%3 == 0){
-            roll_no += " "
-        }
-        roll_no += userData?.euser?.roll_number[i]
-    }
-    let voter_type = "PG";
-    if(userData?.euser?.degree == "B"){
-        voter_type = "UG";
-    }
-    return (
-    <div className={`${styles.cont} p-4 max-w-md ml-auto mr-auto ${hid}`}>
-        <div className={`grid ${styles.grid} gap-5`}>
-            {/* <img src={Icon} className={`${styles.img} max-w-fit`}/> */}
-            <img src={userImg?.img_url} className={`${styles.img} max-w-fit row-span-3`} alt={"Image"} onError={(th)=>{th.target.src = DefaultIMG}}/>
-            <div className={`flex flex-col`}>
-                <div className={`${styles.small}`}>
-                    Name
-                </div>
-                <div className={`${styles.text}`}>
-                    {userData?.euser?.name}
-                </div>
-            </div>
-            <div className={`flex flex-col`}>
-                <div className={`${styles.small}`}>
-                    Roll No
-                </div>
-                <div className={`${styles.text}`}>
-                    {roll_no}
-                </div>
-            </div>
-            
-            <div className={`flex flex-col`}>
-                <div className={`${styles.small}`}>
-                    Voter Type
-                </div>
-                <div className={`${styles.text}`}>
-                    {voter_type} (M)
-                </div>
-            </div>
-            <div className={`flex flex-col`}>
-                <div className={`${styles.small}`}>
-                    Hostel
-                </div>
-                <div className={`${styles.text}`}>
-                    {userData?.euser?.hostel}
-                </div>
-            </div>
-            <div className={`flex flex-col`}>
-                <div className={`${styles.small}`}>
-                    Branch
-                </div>
-                <div className={`${styles.text}`}>
-                    {DEPT[userData?.euser?.branch]}
-                </div>
-            </div>
+    roll_no += userData?.euser?.roll_number[i];
+  }
+  let voter_type = "PG";
+  if (userData?.euser?.degree == "B") {
+    voter_type = "UG";
+  }
+  return (
+    <div className={`${styles.cont} w-full md:w-4/6 p-2 ${hid}`}>
+      <div className={`grid ${styles.grid} gap-4`}>
+        {/* <img src={Icon} className={`${styles.img} max-w-fit`}/> */}
+        <img
+          src={userImg?.img_url}
+          className={`${styles.img} max-w-fit h-full object-cover rounded-lg row-span-3`}
+          alt={"Image"}
+          onError={(th) => {
+            th.target.src = DefaultIMG;
+          }}
+        />
+        <div className={`flex flex-col`}>
+          <div className={`${styles.small}`}>Name</div>
+          <div className={`${styles.text}`}>{userData?.euser?.name}</div>
         </div>
-    </div>);
-}
+        <div className={`flex flex-col`}>
+          <div className={`${styles.small}`}>Roll No</div>
+          <div className={`${styles.text}`}>{roll_no}</div>
+        </div>
 
+        <div className={`flex flex-col`}>
+          <div className={`${styles.small}`}>Voter Type</div>
+          <div className={`${styles.text}`}>{voter_type} (M)</div>
+        </div>
+        <div className={`flex flex-col`}>
+          <div className={`${styles.small}`}>Hostel</div>
+          <div className={`${styles.text}`}>{userData?.euser?.hostel}</div>
+        </div>
+        <div className={`flex flex-col`}>
+          <div className={`${styles.small}`}>Branch</div>
+          <div className={`${styles.text}`}>
+            {DEPT[userData?.euser?.branch]}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default RegisterSuccess;
--- frontend/src/screens/pre_election/AboutScreen.js
+++ frontend/src/screens/pre_election/AboutScreen.js
@@ -4,20 +4,20 @@ import Dates from "../../components/Home/Dates/Dates";
 import Debates from "../../components/Home/Debates/Debates";
 import { Helmet } from "react-helmet";
 import RegisterSuccess from "../../components/Home/RegisterSuccess/RegisterSuccess";
+import { useSelector } from "react-redux";
 
 const AboutScreen = () => {
+  const userData = useSelector((state) => state.auth);
   return (
     <>
-    <div className="px-4">
-      <Helmet>
-        <title>Election Portal</title>
-      </Helmet>
-      <Register />
-      <Dates />
-      {/* <Debates /> */}
-      <RegisterSuccess />
-
-    </div>
+      <div className="px-4">
+        <Helmet>
+          <title>Election Portal</title>
+        </Helmet>
+        {userData ? <RegisterSuccess /> : <Register />}
+        <Dates />
+        {/* <Debates /> */}
+      </div>
     </>
   );
 };
