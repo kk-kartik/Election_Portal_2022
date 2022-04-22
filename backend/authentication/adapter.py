12405f10581f52f782b7ec3687f43e64fa36bcd2
VolDemorT-acer <kunalsolanke1000@gmail.com>
2022-04-22 20:12:17 +0530

    update social adapter

--- backend/authentication/adapter.py
+++ backend/authentication/adapter.py
@@ -4,9 +4,7 @@ from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
     def pre_social_login(self, request, sociallogin): 
-        user = sociallogin.user
-        if user.id:  
-            return          
+        user = sociallogin.user   
         try:
             customer = User.objects.get(email=user.email)  # if user exists, connect the account to the existing account and login
             sociallogin.state['process'] = 'connect'                
