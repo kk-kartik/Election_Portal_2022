4384f0ea4d55c1e3845c2ce988e7c2df0441424a
yashinatu <yashinatu2007@gmail.com>
2022-04-23 02:08:59 +0530

    generate-otp states

--- frontend/src/components/Loading/Loading.js
+++ frontend/src/components/Loading/Loading.js
@@ -3,8 +3,8 @@ import styles from "./Loading.module.css"
 
 const Loader = (props) =>{
     return(
-        <div className="flex items-center w-96 shadow-md px-4 rounded-2xl py-8">
-            <div className="text-xl font-atkinson mx-4 text-gray-700">
+        <div className="flex items-center  rounded-2xl ">
+            <div className="text-lg font-atkinson mx-4 text-gray-700">
                 {props.text}
             </div>
             <div class={styles.loader}>
