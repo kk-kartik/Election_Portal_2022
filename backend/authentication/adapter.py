bd682def92774c5cbbd17ae194efc78ee65c9312
VolDemorT-acer <kunalsolanke1000@gmail.com>
2022-04-22 19:50:19 +0530

    update django adapter

---
+++ backend/authentication/adapter.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+
+class MySocialAccountAdapter(DefaultSocialAccountAdapter):
+    def pre_social_login(self, request, sociallogin): 
+        user = sociallogin.user
+        if user.id:  
+            return          
+        try:
+            customer = User.objects.get(email=user.email)  # if user exists, connect the account to the existing account and login
+            sociallogin.state['process'] = 'connect'                
+            perform_login(request, customer, 'none')
+        except Customer.DoesNotExist:
+            pass
