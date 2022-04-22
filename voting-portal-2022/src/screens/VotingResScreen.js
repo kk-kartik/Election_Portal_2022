1378e914f527f0450e334426e46aa22e7b32776e
Sarthak Bhagwat <sarthak_bhagwat@pop-os.localdomain>
2022-04-22 21:07:51 +0530

    voting flow done till bcn

--- voting-portal-2022/src/screens/VotingResScreen.js
+++ voting-portal-2022/src/screens/VotingResScreen.js
@@ -6,9 +6,17 @@ import { useLocation } from "react-router-dom";
 export const VotingResScreen = () => {
   const location = useLocation();
   console.log("[ye hamari location]", location);
+  const { transaction_id, voter_id, block_id, gas } = location.state;
   return (
     <div>
-      <StatusScreen>{/* <SuccessMessage /> */}</StatusScreen>
+      <StatusScreen>
+        <SuccessMessage
+          transaction_id={transaction_id || "dklfjd"}
+          voter_id={voter_id || "dkfdkfj"}
+          block_id={block_id || "dkfjd"}
+          gas={gas || "dklfd"}
+        />
+      </StatusScreen>
     </div>
   );
 };
