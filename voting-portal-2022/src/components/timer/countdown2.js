29c12d92291534c5536927940a8fd445f0181096
Sarthak Bhagwat <sarthak_bhagwat@pop-os.localdomain>
2022-04-23 08:02:43 +0530

    success timer

---
+++ voting-portal-2022/src/components/timer/countdown2.js
@@ -0,0 +1,17 @@
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+
+const Countdown = () => (
+  <CountdownCircleTimer
+    isPlaying
+    duration={7}
+    colors={["#0eed42", "#0ec939", "#0cb032", "#04661b"]}
+    colorsTime={[10, 6, 3, 0]}
+    size={60}
+    strokeWidth={8}
+    onComplete={() => window.location.replace("/")}
+  >
+    {({ remainingTime }) => remainingTime}
+  </CountdownCircleTimer>
+);
+
+export default Countdown;
