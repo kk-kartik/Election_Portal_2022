b81cb73e4d5d1920f0c7b8724f9f56e686a0266a
VolDemorT-acer <kunalsolanke1000@gmail.com>
2022-04-23 15:21:56 +0530

    Add votercard search field

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -49,7 +49,7 @@ class StatisticAdmin(admin.ModelAdmin):
 @admin.register(VoterCard)
 class StatisticAdmin(admin.ModelAdmin):
     search_fields = ['uniqueid',"voter__user__email"]
-    list_display = ['uniqueid',"unqiueid_email"]
+    list_display = ['uniqueid']
 
 
 
